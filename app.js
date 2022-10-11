97ad0ae7439228c9abd2a098e48e1dff125603a6
Dev Ukay <kingsleykrest@gmail.com>
2022-10-12 13:18:10 +1400

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let plusTwo = document.getElementById('plusTwo');
+let timesThree = document.getElementById('times-three');
+let plusNine = document.getElementById('plus-nine');
+
+let count = 0;
+
+function btnPlusTwo() {
+    count = count + 1;
+    counter.textContent = count + ":"
+}
+
+function btnTimesThree() {
+    count = count * 2;
+    counter.textContent = count + ":"
+}
+
+function btnPlusTen() {
+    count = count + 10; 
+    counter.textContent = count + ":"
+}
+
+
+
+
+/*********************** RIGHT SIDE *************************/
+
+let plusTwoR = document.getElementById('plusTwoR');
+let timesThreeR = document.getElementById('timesThreeR');
+let plusNineR = document.getElementById('plusNineR');
+
+// console.log(plusTwoR)
+
+let countR = 0;
+
+function btnPlusTwoR() {
+    countR = countR + 1;
+    counter2.textContent = countR
+}
+
+
+
